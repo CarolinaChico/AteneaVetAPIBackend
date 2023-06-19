bec7d3fc7e1ec348f0bd9cd0c9e05550cb642c36
Carolina Chico <jcchicom@unal.edu.co>
2023-06-19 18:40:00 -0500

    V1.2

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ class AteneaVetAPI{
         this.adminEspecie = new AdminEspecie();
 
         this.app.use(this.configurarCORS);
-        this.app.use(express.json());
+        this.app.use(express.json()); 
 
         this.app.post("/crear_especie",(req,res)=>{this.adminEspecie.crearEspecie(req,res); });
         this.app.get("/listar_especie",(req,res)=>{this.adminEspecie.listarEspecie(req,res); });
@@ -17,7 +17,7 @@ class AteneaVetAPI{
     }
 
     configurarCORS(req, res, next){
-        res.header("Access-Control-Allow-Origen", "*");
+        res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Methods", "GET, POST");
         res.header("Access-Control-Allow-Headers", "Content-Type");
         next();
